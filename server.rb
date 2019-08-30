609d83e4a31565b3159d757217c5f80c3e953b19
trenyik <trenyik.nikolett1@gmail.com>
2019-08-30 16:13:05 +0100

    created single board template & routes

--- server.rb
+++ server.rb
@@ -14,4 +14,23 @@ class Server < Sinatra::Base
         Board.create(title: params['title'])
         erb :boards, :locals => {boards: Board.all}
     end
+
+    post "/tasks/add" do
+        title = params['title']
+        status = params['status']
+        text = params['text']
+        user_id = params['user_id']
+        board_id = params['board_id']
+
+        board_that_we_found = Board.find(board_id)
+        Task.create(title: title, status: status, text: text, user_id: user_id, board_id: board_id )
+        erb :single_board, :locals => { matching_board: board_that_we_found }
+    end
+    
+    get '/board/:id' do
+        board_id = params['id']
+        board_that_we_found = Board.find_by(id: board_id)
+        erb :single_board, :locals => { matching_board: board_that_we_found }
+    end
+
 end
